072f8637ee1e20f2276cdb312b8c1ee3d09b8544
Gabriela <gabriela_ianoski@estudante.sesisenai.org.br>
2024-03-28 09:47:17 -0300

    Funções das classes bonecos adicionadas

--- culinaria_class.js
+++ culinaria_class.js
@@ -21,16 +21,53 @@ class boneco extends Obj{
     tempo = 0
 
     anim(nome){
+        this.tempo +=1
+        if(this.tempo > 12){
+            this.tempo = 0
+            this.frame += 1
+        }
+        if(this.frame>4){
+            this.frame = 1
+        }
+        this.a = "assets/img/"+nome+this.frame+".png"
     }
     
     atual_boneco(){
+        //atualiza boneco
+        this.x += this.dirX
+        this.y += this.dirY
 
+        // console.log(this.x + ' | ' + this.dirX)
+        // console.log(this.y + ' | ' + this.dirY)
+
+        if(this.x <=2){
+            this.x = 2
+        }if(this.x >= 416){
+            this.x = 416
+        }if(this.y >= 600){
+            this.y = 600
+        }if(this.y <=300){
+            this.y = 300  
+    }
     }
     point(){
-
+        //pontos
+        if((objeto.y>=400)&&(objeto.y <= 500)){
+            return true
+        }else{
+            false
+        }
     }
     colid(){
-
+        //colisão
+        if((this.x < objeto.x + objeto.w)&&
+          (this.x + this.w > objeto.x)&&
+          (this.y < objeto.y + objeto.h)&&
+          (this.y + this.h > objeto.y)){
+                return true
+            }else{
+                false
+            }
     }
 }
 class boneco2 extends boneco{
