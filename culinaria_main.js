c420db96184f0bc63ac968d4744ff3722c9c5e28
Gabriela <gabriela_ianoski@estudante.sesisenai.org.br>
2024-03-28 10:19:23 -0300

    Controle do jogo adicionado

--- culinaria_main.js
+++ culinaria_main.js
@@ -3,10 +3,35 @@ let botaoVoltar = document.getElementById()
 let jogar = true
 
 document.addEventListener('keydown', (e)=>{
-    
+    console.log(e.key)
+    fundo.play()
+    if(e.key === 'a' || e.key === 'ArrowLeft'){
+        personagem.dirX -= 10
+    }
+    else if(e.key === 'd' || e.key === 'ArrowRight'){
+        personagem.dirX += 10
+    }
+    else if(e.key === 'w' || e.key === 'ArrowUp'){
+        personagem.dirY -= 10
+    }
+    else if(e.key === 's' || e.key === 'ArrowDown'){
+        personagem.dirY += 10
+    }
 })
 document.addEventListener('keyup', (e)=>{
-
+    console.log(e.key)
+    if(e.key === 'a'  || e.key === 'ArrowLeft'){
+        personagem.dirX = 0
+    }
+    else if(e.key === 'd' || e.key === 'ArrowRight'){
+        personagem.dirX = 0
+    }
+    else if(e.key === 'w' || e.key === 'ArrowUp'){
+        personagem.dirY = 0
+    }
+    else if(e.key === 's' || e.key === 'ArrowDown'){
+        personagem.dirY = 0
+    }
 })
 function game_over(){
     
